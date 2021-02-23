90f6e5cc24ddc6544a9b27c255670c360712bd6b
Paul Keldsen <Pkeld148@gmail.com>
2021-02-23 13:09:19 -0500

    database working, added homepage container

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./containers/Home/Home"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Hello, world!
-        </p>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          {/* <Route exact path="/restaurants" component={AllRestaurants} />
+          <Route exact path="/restaurants/new" component={NewRestaurant} />
+          <Route exact path="/restaurants/:id" component={SingleRestaurant} />
+          <Route exact path="/restaurants/:id/edit" component={EditRestaurant} /> */}
+        </Switch>
+      </Router>
       </header>
     </div>
   );
--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -3,42 +3,40 @@ const db = require("../models/index");
 
 // This file empties the Positions collection and inserts the books below
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/starketf"
-);
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/starketf");
 
 // buy jan 13, 2021
 // sell jan 27, 2021
 const positionSeed = [
   {
     tickerSymbol: "GME",
-    buyPrice: 100.00, 
-    selPrice: 200.00
+    buyPrice: 100.0,
+    selPrice: 200.0,
   },
-	{
+  {
     tickerSymbol: "AAPL",
-    buyPrice: 500.00, 
-    selPrice: 750.00
+    buyPrice: 500.0,
+    selPrice: 750.0,
   },
-	{
+  {
     tickerSymbol: "TSLA",
-    buyPrice: 3000.00, 
-    selPrice: 4000.00
+    buyPrice: 3000.0,
+    selPrice: 4000.0,
   },
-	{
+  {
     tickerSymbol: "AMC",
-    buyPrice: 100.00, 
-    selPrice: 3.00
+    buyPrice: 100.0,
+    selPrice: 3.0,
   },
-	{
+  {
     tickerSymbol: "TWTR",
-    buyPrice: 200.00, 
-    selPrice: 350.00
+    buyPrice: 200.0,
+    selPrice: 350.0,
   },
-	{
+  {
     tickerSymbol: "NVDA",
-    buyPrice: 1000.00, 
-    selPrice: 2000.00
+    buyPrice: 1000.0,
+    selPrice: 2000.0,
   },
 ];
 
@@ -46,34 +44,29 @@ const userSeed = [
   {
     userName: "Tony Stark",
   },
-	{
+  {
     userName: "Ray Dalio",
   },
 ];
 
+db.Position.remove({})
+  .then(() => db.Position.collection.insertMany(positionSeed))
+  .then((data) => {
+    console.log(data.result.n + " records inserted!");
+    process.exit(0);
+  })
+  .catch((err) => {
+    console.log(err);
+    process.exit(1);
+  });
 
-
-db.Position
-	.remove({})
-	.then(() => db.Position.collection.insertMany(positionSeed))
-	.then( (data) => {
-	console.log(data.result.n + " records inserted!");
-  process.exit(0);
-})
-.catch( (err) => {
-	console.log(err);
-	process.exit(1);
-})
-
-db.User
-	.remove({})
-	.then(() => db.User.collection.insertMany(userSeed))
-	.then( (data) => {
-	console.log(data);
-	console.log(data.result.n + " records inserted!");
-  process.exit(0);
-})
-.catch( (err) => {
-	console.log(err);
-	process.exit(1);
-});
+db.User.remove({})
+  .then(() => db.User.collection.insertMany(userSeed))
+  .then((data) => {
+    console.log(data.result.n + " records inserted!");
+    process.exit(0);
+  })
+  .catch((err) => {
+    console.log(err);
+    process.exit(1);
+  });
