e322421213e7a7ea94adbe3de705f71dd8729bd6
Brian Hestir <brhestir@gmail.com>
2021-02-23 13:39:10 -0500

    Moved seedDB init code into /api/config route

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const routes = require("./routes");
 
 const PORT = process.env.PORT || 3001;
 
+const db = require("./models/index");
+
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
@@ -24,7 +26,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/starketf", {
 });
 
 app.get("/api/config", (req, res) => {
-	res.json( { apiLoopbackOK: true, } );
+	insertSeedData();
+	res.json( { seedDataInserted: true, } );
 });
 
 app.use(routes);
@@ -36,3 +39,70 @@ app.get("*", (req, res) => {
 app.listen(PORT, () => {
 	console.log(`Server running on http://localhost${PORT}`);
 });
+
+
+function insertSeedData() {
+	const positionSeed = [
+		{
+			tickerSymbol: "GME",
+			buyPrice: 100.0,
+			selPrice: 200.0,
+		},
+		{
+			tickerSymbol: "AAPL",
+			buyPrice: 500.0,
+			selPrice: 750.0,
+		},
+		{
+			tickerSymbol: "TSLA",
+			buyPrice: 3000.0,
+			selPrice: 4000.0,
+		},
+		{
+			tickerSymbol: "AMC",
+			buyPrice: 100.0,
+			selPrice: 3.0,
+		},
+		{
+			tickerSymbol: "TWTR",
+			buyPrice: 200.0,
+			selPrice: 350.0,
+		},
+		{
+			tickerSymbol: "NVDA",
+			buyPrice: 1000.0,
+			selPrice: 2000.0,
+		},
+	];
+	
+	const userSeed = [
+		{
+			userName: "Tony Stark",
+		},
+		{
+			userName: "Ray Dalio",
+		},
+	];
+	
+	db.Position.remove({})
+		.then(() => db.Position.collection.insertMany(positionSeed))
+		.then((data) => {
+			console.log(data.result.n + " records inserted!");
+			process.exit(0);
+		})
+		.catch((err) => {
+			console.log(err);
+			process.exit(1);
+		});
+	
+	db.User.remove({})
+		.then(() => db.User.collection.insertMany(userSeed))
+		.then((data) => {
+			console.log(data.result.n + " records inserted!");
+			process.exit(0);
+		})
+		.catch((err) => {
+			console.log(err);
+			process.exit(1);
+		});
+}
