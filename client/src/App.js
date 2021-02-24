805c612cee24708345bbba65b6f1001495250be6
Paul Keldsen <Pkeld148@gmail.com>
2021-02-23 19:56:13 -0500

    added AddPosition container and route, added bulma cdn to index.html, added very bad initial styling to addposition route

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import AddPosition from "./containers/AddPosition/AddPosition";
 import Home from "./containers/Home/Home"
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <Router>
         <Switch>
           <Route exact path="/" component={Home} />
+          <Route exact path="/addposition" component={AddPosition} />
           {/* <Route exact path="/restaurants" component={AllRestaurants} />
           <Route exact path="/restaurants/new" component={NewRestaurant} />
           <Route exact path="/restaurants/:id" component={SingleRestaurant} />
