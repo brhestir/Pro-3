21737d1c555ba4009c5ad2ee9d57867f926e249f
Paul Keldsen <Pkeld148@gmail.com>
2021-02-25 14:24:11 -0500

    stock symbol and current price now displaying on page

--- client/src/containers/AddPosition/AddPosition.jsx
+++ client/src/containers/AddPosition/AddPosition.jsx
@@ -3,10 +3,16 @@ import API from "../../utils/API";
 
 const AddPosition = () => {
   const [searchQuery, setSearchQuery] = useState("GME");
+  
+  // TODO: Be able to pull full stock name from the search query
+  // const [stockName, setStockName] = useState("");
+
+  const [stockPrice, setStockPrice] = useState("");
 
   useEffect(() => {
     API.search(searchQuery).then((res) => {
-      console.log(res);
+      console.log(res.data);
+      setStockPrice(res.data.data[0].last);
       // console.log(res.data.data[0].last);
     });
   }, []);
@@ -35,7 +41,9 @@ const AddPosition = () => {
               />
             </div>
             <button className="button is-rounded is-info">Search</button>
-            <div>Search information will show up here</div>
+            <div>Search information will show up under here:</div>
+            <div>Stock: {searchQuery}</div>
+            <div>Price: {stockPrice}</div>
             <button className="button is-link">Add to Portfolio</button>
           </div>
         </div>
