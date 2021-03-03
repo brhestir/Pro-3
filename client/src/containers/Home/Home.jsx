b363fad565dbff91f68ec7aeb198f7c370eab647
Paul Keldsen <Pkeld148@gmail.com>
2021-03-03 06:51:35 -0500

    signup page restructured

--- client/src/containers/Home/Home.jsx
+++ client/src/containers/Home/Home.jsx
@@ -5,7 +5,7 @@ import "./Home.css";
 const Home = () => {
   return (
     <>
-      <div className="container  center-align">
+      <div className="container center-align">
               <div>
                 <img src={diamondHandsMascot} height="400" />
                 <h3>Have you been diagnosed with a chronic case of "Paper Hands?"</h3>
