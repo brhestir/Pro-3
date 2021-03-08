310abd170fa0cc6d30af2b8cf0509d49dd7b3c41
Brian Hestir <brhestir@gmail.com>
2021-03-08 13:07:56 -0500

    Sidenav set to closed by default, opens on button click on mobile devices, added material-icons to navbar and sidenav buttons

--- client/src/containers/NavbarVanilla/NavbarVanilla.jsx
+++ client/src/containers/NavbarVanilla/NavbarVanilla.jsx
@@ -6,7 +6,7 @@ import M from "materialize-css";
 import sidenavInstances from "../../App.js";
 
 const NavbarVanilla = () => {
-  const { userObject, setUserObject, token, setToken } = useContext(
+  const { userObject, setUserObject, setToken } = useContext(
     GlobalContext
   );
 
@@ -15,7 +15,7 @@ const NavbarVanilla = () => {
 	useEffect(() => {
 		let sidenavElem = document.getElementById('app-sidenav');
 		let sidenavInstances = M.Sidenav.init(sidenavElem, { draggable: true });
-		sidenavInstances.open();
+		// sidenavInstances.open();
 	}, []);
 
 	const logoutBtnClickHandler = () => {
@@ -31,24 +31,24 @@ const NavbarVanilla = () => {
 				<nav>
 					<div className="nav-wrapper teal darken-4" role="navigation" aria-label="main navigation">
 						<Link className="brand-logo center" to="/"><img src={diamondHands} width="300" alt="Diamond Hands Logo"/></Link>
-						<Link className="sidenav-trigger" data-target="app-sidenav" to="#"><i class="material-icons">menu</i></Link>
+						<Link className="sidenav-trigger" data-target="app-sidenav" to="#"><i className="material-icons">menu</i></Link>
 						<ul className="right hide-on-med-and-down">
-							{ userObject._id && ( <li><Link to="/positions/add">Add Position</Link></li> )}
-							{ userObject._id && ( <li><Link to="/positions/all">View Positions</Link></li> )}
-							{ userObject._id && ( <li><Link to="/" onClick={logoutBtnClickHandler}>Logout</Link></li> )}
-							{!userObject._id && ( <li><Link to="/signup">Sign up<i className="material-icons right">person_add</i></Link></li> )}
-							{!userObject._id && ( <li><Link to="/login">Log in<i className="material-icons right">assignment_ind</i></Link></li> )}
+							{ userObject._id && ( <li><Link to="/positions/add">Add Position<i className="material-icons right">add</i></Link></li> )}
+							{ userObject._id && ( <li><Link to="/positions/all">View Positions<i className="material-icons right">view_list</i></Link></li> )}
+							{ userObject._id && ( <li><Link to="/" onClick={logoutBtnClickHandler}>Logout<i className="material-icons right">exit_to_app</i></Link></li> )}
+							{!userObject._id && ( <li><Link to="/signup">Sign up<i className="material-icons left tiny">person_add</i></Link></li> )}
+							{!userObject._id && ( <li><Link to="/login">Log in<i className="material-icons left tiny">assignment_ind</i></Link></li> )}
 						</ul>
 
 					</div>
 				</nav>
 			</div>
 			<ul class="sidenav" id="app-sidenav">
-				{ userObject._id && ( <li><Link className="sidenav-close" to="/positions/add">Add Position</Link></li> )}
-				{ userObject._id && ( <li><Link className="sidenav-close" to="/positions/all">View Positions</Link></li> )}
-				{ userObject._id && ( <li><Link className="sidenav-close" to="/" onClick={logoutBtnClickHandler}>Logout</Link></li> )}
-				{!userObject._id && ( <li><Link className="sidenav-close" to="/signup">Sign up<i className="material-icons right">person_add</i></Link></li> )}
-				{!userObject._id && ( <li><Link className="sidenav-close" to="/login">Log in<i className="material-icons right">assignment_ind</i></Link></li> )}
+				{ userObject._id && ( <li><Link className="sidenav-close" to="/positions/add">Add Position<i className="material-icons left">add</i></Link></li> )}
+				{ userObject._id && ( <li><Link className="sidenav-close" to="/positions/all">View Positions<i className="material-icons left">view_list</i></Link></li> )}
+				{ userObject._id && ( <li><Link className="sidenav-close" to="/" onClick={logoutBtnClickHandler}>Logout<i className="material-icons left">exit_to_app</i></Link></li> )}
+				{!userObject._id && ( <li><Link className="sidenav-close" to="/signup">Sign up<i className="material-icons left">person_add</i></Link></li> )}
+				{!userObject._id && ( <li><Link className="sidenav-close" to="/login">Log in<i className="material-icons left">assignment_ind</i></Link></li> )}
 			</ul>
 		</>
   );
