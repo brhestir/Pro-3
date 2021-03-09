ea1f557b417685c4b12c3dbd903748a8640627a5
Paul Keldsen <Pkeld148@gmail.com>
2021-03-09 14:54:46 -0500

    changed profilecard styling to be less bulky

--- client/src/components/UserProfileCard/UserProfileCard.jsx
+++ client/src/components/UserProfileCard/UserProfileCard.jsx
@@ -11,17 +11,17 @@ const UserProfileCard = (props) => {
       <div className="row">
         <div className="col s12 m6">
           <div className="card teal darken-4">
-            <div className="card-content white-text">
+            <div className="card-content white-text profile-card">
               <div className="row">
-                <div className="col s4 m6">
+                <div className="col s4">
                   <img
                     className="userPic"
                     src={profilePic}
                     alt="Wealthy human instance"
                   />
                 </div>
-                <div className="col s8 m6">
-                  <p>{props.userObject.userName}</p>
+                <div className="col s6 push-s1 profile-card-text">
+                  <h3>{props.userObject.userName}</h3>
                   {props.userObject.totalChange 
                   ? <p>Lifetime Return: {props.userObject.totalChange.toFixed(2)}%</p>
                   : <p>Lifetime Return: 0.00%</p>
