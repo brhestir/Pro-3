e5493029f7ced274a1cc19d45e881dabb2b31e05
Brian Hestir <brhestir@gmail.com>
2021-03-17 06:33:36 -0400

    Single-quotes -> Double-quotes for eslint

--- models/position.js
+++ models/position.js
@@ -1,9 +1,9 @@
 // ./models/position.js
-const mongoose = require(`mongoose`);
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const positionSchema = new Schema(
-	{
+	{	
 		stockFullName: {
 			type: String,
 			trim: true,
@@ -22,29 +22,25 @@ const positionSchema = new Schema(
 	},
 	{
 		timestamps: {
-			createdAt: 'created_at',
-			updatedAt: 'updated_at'
+			createdAt: "created_at",
+			updatedAt: "updated_at"
 		}
 	}
 );
 
 // Thank you Peter
-positionSchema.post('remove', document => {
-  const positionId = document._id;
-  mongoose
-    .model('User')
-    .findOneAndUpdate(
-      { positions: { $in: [positionId] } },
-      { $pull: { positions: positionId } },
-      { new: true }
-    )
-    .then(dbModel => console.log(dbModel))
-    .catch(err => console.log(err));
+positionSchema.post("remove", document => {
+	const positionId = document._id;
+	mongoose
+		.model("User")
+		.findOneAndUpdate(
+			{ positions: { $in: [positionId] } },
+			{ $pull: { positions: positionId } },
+			{ new: true }
+		)
+		.then(dbModel => console.log(dbModel))
+		.catch(err => console.log(err));
 });
 
 const Position = mongoose.model("Position", positionSchema);
-
 module.exports = Position;
-
-
-
